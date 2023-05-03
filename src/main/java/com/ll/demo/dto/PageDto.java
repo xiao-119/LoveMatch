f7e9c018e546ecc098af068b77336a854ca75c46
sakkura618 <sakkura618@gmail.com>
2023-05-03 22:56:53 +0800

    swagger 入参提示

--- src/main/java/com/ll/demo/dto/PageDto.java
+++ src/main/java/com/ll/demo/dto/PageDto.java
@@ -1,21 +1,23 @@
 package com.ll.demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 public class PageDto {
     @Min(value = 1, message = "页码最小为1")
+    @Schema(description = "页码序号")
     Integer pageNo;
 
 
     // @Size(min = 1, max = 10, message = "页码最小为1")
     @Min(value = 1, message = "页大小最小为1")
+    @Schema(description = "单页大小")
     Integer pageSize;
 
 
