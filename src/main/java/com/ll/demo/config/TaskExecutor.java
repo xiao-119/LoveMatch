9b5cd7f27d5b5af05f19bf781c9ecc0dcebc743c
sakkura618 <sakkura618@gmail.com>
2023-05-17 21:44:58 +0800

    WebMvcConfigurer Callable异步处理请求

--- src/main/java/com/ll/demo/config/TaskExecutor.java
+++ src/main/java/com/ll/demo/config/TaskExecutor.java
@@ -1,17 +1,19 @@
 package com.ll.demo.config;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
 @EnableAsync // 开启异步事件支持
-public class TaskExecutor {
+public class TaskExecutor implements WebMvcConfigurer {
     @Bean("asyncThreadPool")
-    public Executor myTaskExecutor() {
+    public ThreadPoolTaskExecutor myTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(4);
@@ -20,5 +22,9 @@ public class TaskExecutor {
         executor.setThreadNamePrefix("MyTaskExecutor-");
         executor.initialize();
         return executor;
+    }
+
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor(myTaskExecutor());
     }
 }
--- src/main/java/com/ll/demo/controller/TestController.java
+++ src/main/java/com/ll/demo/controller/TestController.java
@@ -12,22 +12,32 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
 @Tag(name = "测试接口",description = "测试接口description")
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
 
-
-
-    @Operation(summary = "测试接口hello",description = "测试接口description",tags = {"测试接口tags","测试接口tags2"})
+    @Operation(summary = "测试接口hello", description = "测试接口description", tags = {"测试接口tags", "测试接口tags2"})
     @ApiResponses({
-            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = User.class), mediaType = "application/json") }),
-            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
-            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = User.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
 
     @RequestMapping("/test")
     public R<?> testGet() {
         return R.success("Hello World");
+    }
+
+
+    @RequestMapping("/testAsync")
+    public Callable<String> callable() {
+        return () -> {
+            TimeUnit.SECONDS.sleep(10);
+            return "async";
+        };
     }
 }
