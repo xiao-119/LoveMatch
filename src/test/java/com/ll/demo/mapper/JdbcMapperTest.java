7ba428486f28b8035beeb099949c845c7e3a5685
sakkura618 <sakkura618@gmail.com>
2023-06-01 23:33:05 +0800

    JdbcTemplate 获取datasource,并且查看url root password

--- src/test/java/com/ll/demo/mapper/JdbcMapperTest.java
+++ src/test/java/com/ll/demo/mapper/JdbcMapperTest.java
@@ -1,11 +1,16 @@
 package com.ll.demo.mapper;
 
 import com.baomidou.mybatisplus.test.autoconfigure.AutoConfigureMybatisPlus;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @SpringBootTest
@@ -15,8 +20,26 @@ public class JdbcMapperTest {
     @Autowired
     private JdbcMapper jdbcMapper;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
+    @Disabled
     public void testSelectAll() {
         log.info("{}", jdbcMapper.selectAll("user"));
+    }
+
+
+
+    // skip 跳过测试
+    @Test
+    public void queryDataSource(){
+        DataSource dataSource = jdbcTemplate.getDataSource();
+        System.out.println(dataSource);
+        if (dataSource instanceof HikariDataSource hikariDataSource){
+            System.out.println(hikariDataSource.getJdbcUrl());
+            System.out.println(hikariDataSource.getUsername());
+            System.out.println(hikariDataSource.getPassword());
+        }
     }
 }
